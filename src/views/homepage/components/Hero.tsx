3abce87e0e1b02edac967087c30d833f9351e25e
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-03-14 22:20:53 -0400

    typewriter effect
    This is the typewriter. Let me know what you think

--- src/views/homepage/components/Hero.tsx
+++ src/views/homepage/components/Hero.tsx
@@ -3,14 +3,14 @@ import { Jumbotron, Container } from "reactstrap";
 const Hero = () => {
     return (
         <>
-        <Jumbotron className="title" fluid>
-          <Container fluid>
-            <h1 className="display-3"> teachers.who.code </h1>
-            <p className="lead"> Helping teachers succeed in and out of the classroom using tech.</p>
-          </Container>
-        </Jumbotron>
-        
-      </>
+            <Jumbotron className="title" fluid>
+                <Container fluid>
+                    <h1 className="display-3"> teachers.who.code. </h1>
+                    <p className="lead"> Helping teachers succeed in and out of the classroom using tech.</p>
+                </Container>
+            </Jumbotron>
+
+        </>
     );
 }
 
