2e51d9d78339a31c59bc3bc79120ed8a09409ab0
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-03-16 22:14:13 -0400

    meet the team

---
+++ src/views/homepage/components/MeetTeam.tsx
@@ -0,0 +1,15 @@
+import { Container } from "reactstrap";
+
+const MeetTeam = () => {
+    return (
+        <>
+                <Container>
+                    <h1 className="display-3"> Meet our teachers! </h1>
+                    <p className="lead"> Lorem Ipsum </p>
+                </Container>
+
+        </>
+    );
+}
+
+export default MeetTeam;
