67164801413a5f1b62bb2ab0c8eb02379acd809b
Allie Pistolessi <a.pistol17@gmail.com>
2022-03-17 10:24:29 -0400

    Created Nav Bar

--- src/layout/components/TopNavbar.tsx
+++ src/layout/components/TopNavbar.tsx
@@ -1,10 +1,83 @@
-import React from "react";
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+class TopNavbar extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
 
-const TopNavbar = ({ props }: any) => {
-  // const [isOpen, setIsOpen] = useState(false);
-
-  // const toggle = () => setIsOpen(!isOpen);
-
-  return <div>Navbar Practice</div>;
-};
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar fixed='top' dark expand="md">
+          <NavbarBrand href="/">t.w.c logo</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Home</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar className="dropdown">
+                <DropdownToggle nav onHover>
+                  Path
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>
+                    Path 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Path 2
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <UncontrolledDropdown nav inNavbar className="dropdown">
+                <DropdownToggle nav onHover>
+                  Resources
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>
+                    Resource 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Resource 2
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Blog</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">FAQ</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Contact</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
 export default TopNavbar;
+
