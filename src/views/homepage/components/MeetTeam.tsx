de57d8f12eb2114b769b2556b68d0ff3732e38ca
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-03-19 16:46:05 -0400

    Update MeetTeam.tsx

--- src/views/homepage/components/MeetTeam.tsx
+++ src/views/homepage/components/MeetTeam.tsx
@@ -8,48 +8,48 @@ const MeetTeam = () => {
         <>
             <CardDeck>
                 <Card>
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" class="pict" alt="Card image cap" />
                     <CardBody>
-                        <CardTitle>Card title</CardTitle>
+                        <CardTitle>Clara Harmonson</CardTitle>
                         <CardSubtitle>Card subtitle</CardSubtitle>
                         <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                        <Button>Button</Button>
+                        <Button>Contact Clara</Button>
                     </CardBody>
                 </Card>
                 <Card>
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" class="pict" alt="Card image cap" />
                     <CardBody>
-                        <CardTitle>Card title</CardTitle>
+                        <CardTitle>Marilyn Marquez</CardTitle>
                         <CardSubtitle>Card subtitle</CardSubtitle>
                         <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                        <Button>Button</Button>
+                        <Button>Contact Marilyn</Button>
                     </CardBody>
                 </Card>
                 <Card>
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" class="pict" alt="Card image cap" />
                     <CardBody>
-                        <CardTitle>Card title</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardTitle>Carla Montano</CardTitle>
+                        <CardSubtitle>Web DEveloper</CardSubtitle>
                         <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                        <Button>Button</Button>
+                        <Button>Contact Carla</Button>
                     </CardBody>
                 </Card>
                 <Card>
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"  class="pict" alt="Card image cap" />
                     <CardBody>
-                        <CardTitle>Card title</CardTitle>
+                        <CardTitle>Allison Pistorelli</CardTitle>
                         <CardSubtitle>Card subtitle</CardSubtitle>
                         <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                        <Button>Button</Button>
+                        <Button>Contact Allison</Button>
                     </CardBody>
                 </Card>
                 <Card>
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" class="pict" alt="Card image cap" />
                     <CardBody>
-                        <CardTitle>Card title</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardTitle>Michael Rivera</CardTitle>
+                        <CardSubtitle>Web Developer</CardSubtitle>
                         <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-                        <Button>Button</Button>
+                        <Button>Contact Michael</Button>
                     </CardBody>
                 </Card>
             </CardDeck>
