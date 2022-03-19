7ee2149d991e99f0d5b0fff81cdeaf66359581a1
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-03-19 16:19:06 -0400

    Added cards for Meet the Team
    Meet the Team (in progress)

--- src/views/homepage/HomePage.tsx
+++ src/views/homepage/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
+import MeetTeam from "./components/MeetTeam";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <MeetTeam/>
     </>
   );
 };
