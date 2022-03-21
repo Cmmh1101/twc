65b7a053adc78758b64b4581fc23c7135f3550e4
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-03-21 12:06:57 -0500

    added arrow image and animations, fixed title animation duration

--- src/utils/ArrowDown.tsx
+++ src/utils/ArrowDown.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ArrowDown = () => {
+  const scrollTo = () => {
+    const anchor = document.querySelector("#twc-intro");
+    anchor?.scrollIntoView({ behavior: "smooth", block: "center" });
+  };
+
   return (
-    <Link to="/">
-      <div className="hero-arrow">
-        <img src="../../../images/arrowDown.png" alt="arrow down" />
-      </div>
-    </Link>
+    <div
+      className="hero-arrow mt-5"
+      onClick={() => {
+        scrollTo();
+      }}
+    >
+      <img src="../../../images/arrowDown.png" alt="arrow down" />
+    </div>
   );
 };
 
