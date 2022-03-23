dfb9e9953da74eecea3a1d6a143519d9c69088da
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-03-22 21:51:57 -0500

    relocated resources section file

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,19 +3,14 @@ import { Container } from "reactstrap";
 import AppRouter from "../router/AppRouter";
 import Footer from "./components/Footer";
 import TopNavbar from "./components/TopNavbar";
-import Resources from "./components/Resources";
 
-interface Props { }
-
-const Layout = (props: Props) => {
+const Layout = () => {
   return (
     <>
       <TopNavbar />
       <main className="main">
         <Container fluid className="p-0">
           <AppRouter />
-          <Resources />
-
         </Container>
       </main>
       <Footer />
