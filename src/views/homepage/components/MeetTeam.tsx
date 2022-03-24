632cc94ca302c3e6d3d3656ac1073db3a2642844
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-03-24 15:05:35 -0400

    team cards
    Updated CSS team cards

--- src/views/homepage/components/MeetTeam.tsx
+++ src/views/homepage/components/MeetTeam.tsx
@@ -1,63 +1,134 @@
+import { Link } from "react-router-dom";
 import {
-    Card, Button, CardImg, CardTitle, CardText, CardDeck,
-    CardSubtitle, CardBody
-} from 'reactstrap';
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardDeck,
+  CardSubtitle,
+  CardBody,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+import MainHeader from "../../../utils/MainHeader";
 
 const MeetTeam = () => {
-    return (
-        <>
-            <h1 className="MeetTheTeam">Meet the Team</h1>
-            <CardDeck>
-
-                <Card className="teamCard">
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" className="pict" alt="Card image cap" />
-                    <CardBody className="cardBody">
-                        <CardTitle className="teamMember">Clara Harmonson</CardTitle>
-                        <CardSubtitle className="jobTitle">Card subtitle</CardSubtitle>
-                        <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                        <Button className="BtnTeam">Read Clara's story</Button>
-                    </CardBody >
-                </Card>
-                <Card className="teamCard">
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" className="pict" alt="Card image cap" />
-                    <CardBody className="cardBody">
-                        <CardTitle className="teamMember">Marilyn Marquez</CardTitle>
-                        <CardSubtitle className="jobTitle">Card subtitle</CardSubtitle>
-                        <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                        <Button className="BtnTeam">Read Marilyn's story</Button>
-                    </CardBody>
-                </Card>
-                <Card className="teamCard">
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" className="pict" alt="Card image cap" />
-                    <CardBody className="cardBody">
-                        <CardTitle className="teamMember">Carla Montano</CardTitle>
-                        <CardSubtitle className="jobTitle">Web Developer</CardSubtitle>
-                        <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                        <Button className="BtnTeam">Read Carla's story</Button>
-                    </CardBody>
-                </Card>
-                <Card className="teamCard">
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" className="pict" alt="Card image cap" />
-                    <CardBody className="cardBody">
-                        <CardTitle className="teamMember">Allie Pistolessi</CardTitle>
-                        <CardSubtitle className="jobTitle">Web Developer</CardSubtitle>
-                        <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                        <Button className="BtnTeam">Read Allie's story</Button>
-                    </CardBody>
-                </Card>
-                <Card className="teamCard">
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" className="pict" alt="Card image cap" />
-                    <CardBody className="cardBody">
-                        <CardTitle className="teamMember">Michael Rivera</CardTitle>
-                        <CardSubtitle className="jobTitle">Web Developer</CardSubtitle>
-                        <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-                        <Button className="BtnTeam">Read Michael's story</Button>
-                    </CardBody >
-                </Card>
-            </CardDeck>
-
-        </>
-    );
-}
+  return (
+  
+     <Container>
+      <MainHeader headerText="Meet The Team" />
+      <CardDeck>
+        <Card className="team-card">
+          <CardImg
+            top
+            width="100%"
+            src="https://st3.depositphotos.com/6672868/13801/v/600/depositphotos_138013506-stock-illustration-user-profile-group.jpg"
+            className="pict"
+            alt="Card image cap"
+          />
+          <CardBody className="card-body">
+            <CardTitle className="team-member">Clara Harmonson</CardTitle>
+            <CardSubtitle className="job-title">Card subtitle</CardSubtitle>
+            <CardText>
+            This card has supporting text below as a natural lead-in to
+              additional content.
+            </CardText>
+            <Link to="/">
+                Read Clara's Story
+              </Link>
+          </CardBody>
+        </Card>
+        <Card className="team-card">
+          <CardImg
+            top
+            src="https://st3.depositphotos.com/6672868/13801/v/600/depositphotos_138013506-stock-illustration-user-profile-group.jpg"
+            className="pict"
+            alt="Card image cap"
+          />
+          <CardBody className="card-body">
+            <CardTitle className="team-member">Marilyn Marquez</CardTitle>
+            <CardSubtitle className="job-title">Card subtitle</CardSubtitle>
+            <CardText>
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </CardText>
+            <Link to="/">
+                Read Marilyn's Story
+              </Link>
+          </CardBody>
+        </Card>
+        <Card className="team-card">
+          <CardImg
+            top
+            src="https://st3.depositphotos.com/6672868/13801/v/600/depositphotos_138013506-stock-illustration-user-profile-group.jpg"
+            className="pict"
+            alt="Card image cap"
+          />
+          <CardBody className="card-body">
+            <CardTitle className="team-member">Carla Montano</CardTitle>
+            <CardSubtitle className="job-title">Web Developer</CardSubtitle>
+            <CardText>
+            This card has supporting text below as a natural lead-in to
+              additional content.
+            </CardText>
+            <Link to="/">
+                Read Carla's Story
+              </Link>
+          </CardBody>
+        </Card>
+        <Card className="team-card">
+          <CardImg
+            top
+            
+            src="https://st3.depositphotos.com/6672868/13801/v/600/depositphotos_138013506-stock-illustration-user-profile-group.jpg"
+            className="pict"
+            alt="Card image cap"
+          />
+          <CardBody className="card-body">
+            <CardTitle className="team-member">Allie Pistolessi</CardTitle>
+            <CardSubtitle className="job-title">Web Developer</CardSubtitle>
+            <CardText>
+            This card has supporting text below as a natural lead-in to
+              additional content.
+            </CardText>
+            <Link to="/">
+                Read Allie's Story
+              </Link>
+          </CardBody>
+        </Card>
+        <Card className="team-card">
+          <CardImg
+            top
+            src="https://st3.depositphotos.com/6672868/13801/v/600/depositphotos_138013506-stock-illustration-user-profile-group.jpg"
+            className="pict"
+            alt="Card image cap"
+          />
+          <CardBody className="card-body">
+            <CardTitle className="team-member">Michael Rivera</CardTitle>
+            <CardSubtitle className="job-title">Web Developer</CardSubtitle>
+            <CardText>
+            This card has supporting text below as a natural lead-in to
+              additional content.
+            </CardText>
+            <Link to="/">
+                Read Michael's Story
+              </Link>
+          </CardBody>
+        </Card>
+      </CardDeck>
+      <Row>
+          <Col className="d-flex justify-content-center my-5">
+          <Button className="resources-button mb-5 mb-md-0 mx-auto">
+                Meet more teachers who code!
+              </Button>
+          </Col>
+      </Row>
+     
+      </Container>
+   
+  );
+};
 
 export default MeetTeam;
