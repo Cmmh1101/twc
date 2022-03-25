8ec993454a8ce9600c574c7f672be8c03ca0c7f1
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-03-24 19:07:07 -0500

    added new logo, deleted unused logos

--- src/layout/components/TopNavbar.tsx
+++ src/layout/components/TopNavbar.tsx
@@ -11,6 +11,7 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
+  Container,
 } from "reactstrap";
 
 const TopNavbar = () => {
@@ -21,57 +22,59 @@ const TopNavbar = () => {
   };
 
   return (
-    <Navbar tag="nav" className="navbar" dark expand="md">
-      <NavbarBrand className="mr-auto" href="/">
-        <img
-          src="/twcLogo2.jpeg"
-          className="navbar-image"
-          height="100"
-          width="100"
-          alt="Teachers Who Code logo"
-        />
-      </NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem className="px-3">
-            <NavLink href="/components/">Home</NavLink>
-          </NavItem>
-          <UncontrolledDropdown nav inNavbar className="dropdown">
-            <DropdownToggle nav className="px-3">
-              Path
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem>Path 1</DropdownItem>
-              <DropdownItem>Path 2</DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-          <UncontrolledDropdown nav inNavbar className="dropdown">
-            <DropdownToggle nav className="px-3">
-              Resources
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem>Resource 1</DropdownItem>
-              <DropdownItem>Resource 2</DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-          <NavItem className="px-3">
-            <NavLink href="https://github.com/reactstrap/reactstrap">
-              Blog
-            </NavLink>
-          </NavItem>
-          <NavItem className="px-3">
-            <NavLink href="https://github.com/reactstrap/reactstrap">
-              FAQ
-            </NavLink>
-          </NavItem>
-          <NavItem className="px-3">
-            <NavLink href="https://github.com/reactstrap/reactstrap">
-              Contact
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
+    <Navbar tag="nav" fixed="top" className="navbar shadow" dark expand="md">
+      <Container>
+        <NavbarBrand className="mr-auto" href="/">
+          <img
+            src="/logo.png"
+            className="navbar-image"
+            height="100"
+            width="100"
+            alt="Teachers Who Code logo"
+          />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem className="px-3">
+              <NavLink href="/components/">Home</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar className="dropdown">
+              <DropdownToggle nav className="px-3">
+                Path
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem>Path 1</DropdownItem>
+                <DropdownItem>Path 2</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <UncontrolledDropdown nav inNavbar className="dropdown">
+              <DropdownToggle nav className="px-3">
+                Resources
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem>Resource 1</DropdownItem>
+                <DropdownItem>Resource 2</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem className="px-3">
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                Blog
+              </NavLink>
+            </NavItem>
+            <NavItem className="px-3">
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                FAQ
+              </NavLink>
+            </NavItem>
+            <NavItem className="px-3">
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                Contact
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
     </Navbar>
   );
 };
