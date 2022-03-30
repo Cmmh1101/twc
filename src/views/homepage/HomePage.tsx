b05e84d6502957409e554b9d30c95d4c0e0374e8
Allie Pistolessi <a.pistol17@gmail.com>
2022-03-29 21:35:33 -0400

    Created blog post list section

--- src/views/homepage/HomePage.tsx
+++ src/views/homepage/HomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero";
 import IntroTWC from "./components/IntroTWC";
 import Resources from "./components/Resources";
+import BlogPostList from "./components/BlogPostList";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Hero />
       <IntroTWC />
       <Resources />
+      <BlogPostList />
     </>
   );
 };
