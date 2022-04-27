1bd7aeeeeec4282d5bab50e5adb7387318193e47
alliepi <79091099+alliepi@users.noreply.github.com>
2022-04-26 21:02:03 -0400

    Created tech paths page, folder and hero. Also updated App Router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import NotFoundPage from "../views/404page";
 import HomePage from "../views/homepage/HomePage";
+import TechPaths from "../views/techpaths/TechPaths";
 
 const AppRouter = () => {
   return (
     <>
       <Switch>
         <Route path="/" component={HomePage} exact />
+        <Route path="/techpaths" component={TechPaths} exact />
 
         {/* all new routes should be added above */}
         <Route component={NotFoundPage} />
