2f56020717ac9e98e8a7a9b684ef372c84f18854
Charmonson <77593573+Charmonson@users.noreply.github.com>
2022-07-04 16:25:09 -0500

    description small card

--- src/views/resourcespage/components/ResourceDescription.tsx
+++ src/views/resourcespage/components/ResourceDescription.tsx
@@ -1,81 +1,108 @@
 import React from "react";
-import { Button, Col, Container, Row } from "reactstrap";
-import {useTheme} from "../../../provider/ThemeModeProvider";
+import { Button, Card, CardBody, CardTitle, CardSubtitle, CardImg, Col, Container, Row, CardGroup } from "reactstrap";
+import { useTheme } from "../../../provider/ThemeModeProvider";
+import MainHeader from "../../../utils/MainHeader";
 
 
 
-function ResourceDescription ({ ...ResourceData}) {
+function ResourceDescription({ ...ResourceData }) {
     const { englishMode } = useTheme();
 
     return (
         <section className="py-5">
-            <Container className="mb-5" id="twc-intro">
+            <Container>
                 <Row>
-                    {englishMode ? (
-                        <>
-                        <h1 className="resource-title">
-                            {ResourceData.titleSpanish}
-                        </h1>
-                        </>
-                    ) : (
-                        <>
-                        <h1 className="resource-title">
-                            {ResourceData.titleEnglish}
-                        </h1>
-                        </>
-                    ) }
+                    <MainHeader headerText="Our Javascript Favorites" />
                 </Row>
-                <Row>
+            </Container>
+            <Container>
+            <CardGroup className="d-flex align-items-center justify-content-center flex-wrap mb-5">
+                <Col xs={12} md={4}>
+                            <Card style={{ border: "none" }}>
+                                <CardImg
+                                    alt={ResourceData.descriptionImageAltText}
+                                    src={ResourceData.image}
+                                    top width="100%"
+                                    className="card-image" />
+                                <CardBody>
+                                    <CardTitle tag="h5">
+                                        Post Title 1
+                                    </CardTitle>
+                                    <CardSubtitle
+                                        className="mb-2 text-muted"
+                                        tag="h6">
+                                        {englishMode ? (
+                                            <>
+                                                <p> {ResourceData.DescriptionSpanish} </p>
 
-                    <Col
-                        xs={12}
-                        className="d-flex align-items-center
-                        justify-content-center flex-wrap mb-5">
-                    </Col>
-                    <Col xs={12} md={12} className="px-0">
-                        <div className="intro-img">
-                            <img
-                                src={ResourceData.image}
-                                alt={ResourceData.descriptionImageAltText}/>
-                        </div>
-                    </Col>
-                    <Col xs={12} md={12} className="mt-4 px-5">
-                            {englishMode ? (
-                                <>
-                                    <p> {ResourceData.DescriptionSpanish} </p>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <p> {ResourceData.DescriptionEnglish} </p>
 
-                                </>
-                            ) : (
-                                <>
-                                    <p> {ResourceData.DescriptionEnglish} </p>
+                                            </>
+                                        )}
 
-                                </>
-                            )}
+                                    </CardSubtitle>
+                                </CardBody>
+                            </Card>
+                            <Card style={{ border: "none" }}>
+                                <CardImg
+                                    alt={ResourceData.descriptionImageAltText}
+                                    src={ResourceData.image}
+                                    top width="100%"
+                                    className="card-image" />
+                                <CardBody>
+                                    <CardTitle tag="h5">
+                                        Post Title 1
+                                    </CardTitle>
+                                    <CardSubtitle
+                                        className="mb-2 text-muted"
+                                        tag="h6">
+                                        {englishMode ? (
+                                            <>
+                                                <p> {ResourceData.DescriptionSpanish} </p>
 
-                    </Col>
-                    <Col xs={12} md={7} className="d-flex align-items-center justify-content-center">
-                        <Button className="resources-button m-5 mb-md-0">
-                            {englishMode ? (
-                                <>
-                                    Busca los recursos
+                                            </>
+                                        ) : (
+                                            <>
+                                                <p> {ResourceData.DescriptionEnglish} </p>
 
-                                </>
-                            ) : (
-                                <>
-                                    Find resources
+                                            </>
+                                        )}
+                                    </CardSubtitle>
+                                </CardBody>
+                            </Card>
+            </Col>
+        </CardGroup>
 
-                                </>
-                            )
-                            }
-                        </Button>
-                    </Col>                
-                </Row>
+         
+              
+                    
+                    </Container>
               
                
-            </Container>
+                <Col xs={12} md={7} className="d-flex align-items-center justify-content-center">
+                            <Button className="resources-button m-5 mb-md-0">
+                                {englishMode ? (
+                                    <>
+                                        Busca los recursos
+
+                                    </>
+                                ) : (
+                                    <>
+                                        Find resources
+
+                                    </>
+                                )}
+                            </Button>
+                </Col>
+                
+
+           
         </section>
     );
-};
+}
 
 export default ResourceDescription;
 
