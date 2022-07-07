068b9cf509d159a900f66084c313bf754c3c355d
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-07-06 21:13:33 -0500

    added dark mode to layout

--- src/layout/LayoutV2.tsx
+++ src/layout/LayoutV2.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import Footer from "./../layout/components/Footer";
 import TopNavbar from "./../layout/components/TopNavbar";
 import AppRouter from "../router/AppRouter";
+import { useTheme } from "../provider/ThemeModeProvider";
 
 const AppLayout = () => {
+  const { dark, light, darkMode } = useTheme();
+
   return (
     <>
       <TopNavbar />
-      <AppRouter />
+      <main
+        className="main"
+        style={
+          darkMode
+            ? { color: light, backgroundColor: dark }
+            : { color: dark, backgroundColor: light }
+        }
+      >
+        <AppRouter />
+      </main>
       <Footer />
     </>
   );
