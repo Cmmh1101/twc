73da9b5994c10e30ceb69a68db300f9d6e6aaa34
Carla Montano <cm.emprendiendo@gmail.com>
2022-11-04 12:49:21 -0500

    removed unused import

--- src/views/techpaths/components/TechPathsHero.tsx
+++ src/views/techpaths/components/TechPathsHero.tsx
@@ -1,29 +1,25 @@
 import React from "react";
 import { Jumbotron, Container } from "reactstrap";
 import { useTheme } from "../../../provider/ThemeModeProvider";
-import ArrowDown from "../../../utils/ArrowDown";
+// import ArrowDown from "../../../utils/ArrowDown";
 
 const TechPathsHero = () => {
-    const { englishMode } = useTheme();
-    return (
-        <>
-            <Jumbotron className="title" fluid>
-                <Container fluid>
-                    {englishMode ? (
-                        <>
-                            <h1 className="hero-title"> Carreras Tecnológicas Principales</h1>
-
-                        </>
-                    ) : (
-                        <>
-                            <h1 className="hero-title"> Main Tech Routes</h1>
-
-                        </>
-                    )}
-                </Container>
-            </Jumbotron>
-        </>
-    );
+  const { englishMode } = useTheme();
+  return (
+    <Jumbotron className="title" fluid>
+      <Container fluid>
+        {englishMode ? (
+          <>
+            <h1 className="hero-title"> Carreras Tecnológicas Principales</h1>
+          </>
+        ) : (
+          <>
+            <h1 className="hero-title"> Main Tech Routes</h1>
+          </>
+        )}
+      </Container>
+    </Jumbotron>
+  );
 };
 
 export default TechPathsHero;
